e0eb5b01e5825bbda345b9c6b39c5bc3d12db832
Bruno Flores <bruno@brunoflores.com.br>
2025-12-21 07:01:12 +1000

    Add expressions to ast.

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,1 +1,15 @@
- 
+type range = { start_pos : Lexing.position; end_pos : Lexing.position }
+type ty = TInt | TBool | TArrow of ty * ty
+
+type expr = { node : expr_node; range : range }
+
+and expr_node =
+  | EVar of string
+  | EInt of int
+  | EBool of bool
+  | EIf of expr * expr * expr
+  | ELam of string * ty * expr (* fun (x : ty) -> body *)
+  | EApp of expr * expr
+  | ELet of string * expr * expr (* let x = e1 in e2 *)
+  | ELetStack of string * expr * expr (* letstack x = e1 in e2 *)
+  | EExport of expr (* export e *)
